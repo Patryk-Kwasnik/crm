c5707ef0e50d235edb3a1ed8c9e21e03ff9f2e56
PatrykKwasnik <kwasnikzse@interia.pl>
2025-07-27 15:55:19 +0200

    panel login css

--- resources/views/admin/auth/login.blade.php
+++ resources/views/admin/auth/login.blade.php
@@ -1,115 +1,104 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <head>
-  <meta charset="utf-8" />
-  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-  <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <head>
+        <meta charset="utf-8"/>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
+        <meta name="viewport" content="width=device-width, initial-scale=1"/>
 
-  <title></title>
+        <title></title>
 
-  <!-- GOOGLE FONTS -->
-  <link href="https://fonts.googleapis.com/css?family=Montserrat:400,500|Poppins:400,500,600,700|Roboto:400,500" rel="stylesheet"/>
-  <link href="https://cdn.materialdesignicons.com/3.0.39/css/materialdesignicons.min.css" rel="stylesheet" />
+        <!-- GOOGLE FONTS -->
+        <link href="https://fonts.googleapis.com/css?family=Montserrat:400,500|Poppins:400,500,600,700|Roboto:400,500"
+              rel="stylesheet"/>
 
-  <!-- PLUGINS CSS STYLE -->
-  
-  <link href="{{ asset('admin/assets/plugins/toaster/toastr.min.css') }}" rel="stylesheet" />
-  <link href="{{ asset('admin/assets/plugins/nprogress/nprogress.css') }}" rel="stylesheet" />
-  <link href="{{ asset('admin/assets/plugins/flag-icons/css/flag-icon.min.css') }}" rel="stylesheet"/>
-  <link href="{{ asset('admin/assets/plugins/jvectormap/jquery-jvectormap-2.0.3.css') }}" rel="stylesheet" />
-  <link href="{{ asset('admin/assets/plugins/ladda/ladda.min.css') }}" rel="stylesheet" />
-  <link href="{{ asset('admin/assets/plugins/select2/css/select2.min.css') }}" rel="stylesheet" />
-  <link href="{{ asset('admin/assets/plugins/daterangepicker/daterangepicker.css') }}" rel="stylesheet" />
-
-  <!-- SLEEK CSS -->
-  <link id="sleek-css" rel="stylesheet" href="{{ asset('admin/assets/css/sleek.css') }}" />
-
-  
-
-  <!-- FAVICON -->
-  <link href="assets/img/favicon.png" rel="shortcut icon" />
-
-  <!--
-    HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries
-  -->
-  <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-  <!--[if lt IE 9]>
-    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-  <![endif]-->
-  <script src="{{ asset('admin/assets/plugins/nprogress/nprogress.js') }}"></script>
+        <!-- FAVICON -->
+        <link href="assets/img/favicon.png" rel="shortcut icon"/>
+        @vite(['resources/sass/admin.scss'])
+    </head>
 </head>
-
-</head>
-  <body class="bg-light-gray" id="body">
-      <div class="container d-flex flex-column justify-content-between vh-100">
-      <div class="row justify-content-center mt-5">
+<body id="body">
+<div class="container d-flex flex-column justify-content-between vh-100">
         <div class="col-xl-5 col-lg-6 col-md-10">
-          <div class="card">
-            <div class="card-header bg-primary">
-              <div class="app-brand">
-                <a href="/index.html">
-                  <svg class="brand-icon" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid" width="30" height="33"
-                    viewBox="0 0 30 33">
-                    <g fill="none" fill-rule="evenodd">
-                      <path class="logo-fill-blue" fill="#7DBCFF" d="M0 4v25l8 4V0zM22 4v25l8 4V0z" />
-                      <path class="logo-fill-white" fill="#FFF" d="M11 4v25l8 4V0z" />
-                    </g>
-                  </svg>
-                  <span class="brand-name">Dashboard</span>
-                </a>
-              </div>
-            </div> 
+            <div class="card login-admin">
 
-            
-            @if(session('success'))
-     <div class="alert alert-success alert-dismissible fade show" role="alert">
-  <strong>{{ session('success') }}</strong>  
-  <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-    <span aria-hidden="true">&times;</span>
-  </button>
-   </div>
-   @endif
-            <div class="card-body p-5">
-
-              <h4 class="text-dark mb-5">Sign In</h4>
-              <form method="POST" action="{{ route('admin.login') }}">
-               @csrf
-                <div class="row">
-                  <div class="form-group col-md-12 mb-4">
-                    <input type="email" name="email" class="form-control input-lg"   aria-describedby="emailHelp" placeholder="Email">
-                  </div>
-                  <div class="form-group col-md-12 ">
-                    <input type="password" class="form-control input-lg" name="password"  placeholder="Password">
-                  </div>
-                  <div class="col-md-12">
-                    <div class="d-flex my-2 justify-content-between">
-                      <div class="d-inline-block mr-3">
-                        <label class="control control-checkbox">Remember me
-                          <input type="checkbox" />
-                          <div class="control-indicator"></div>
-                        </label>
-                
-                      </div>
-     <p><a class="text-blue" href="{{ route('password.request') }}">Forgot Your Password?</a></p>
+                @if(session('success'))
+                    <div class="alert alert-success alert-dismissible fade show" role="alert">
+                        <strong>{{ session('success') }}</strong>
+                        <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
                     </div>
-                    <button type="submit" class="btn btn-lg btn-primary btn-block mb-4">Sign In</button>
-                    <p>Don't have an account yet ?
-       <a class="text-blue" href="{{ route('register') }}">Sign Up</a>
-                    </p>
-                  </div>
+                @endif
+                <div class="hero">
+                    <div class="main-box">
+                        <div class="form-box">
+                            <span class="brand-name">CRM Admin</span>
+
+                            <div class="button-box">
+                                <div id="btn"></div>
+                                <button id="log" type="button" class="toggle-btn" onclick="login()">Log in</button>
+                                <button id="reg" type="button" class="toggle-btn" onclick="register()">Register</button>
+                            </div>
+                            <form method="POST" action="{{ route('admin.login') }}" id="login" class="input-group" >
+                                @csrf
+                                <input type="email" name="email" class="input-field"
+                                       aria-describedby="emailHelp" placeholder="Email">
+                                <input id="pwd" type="password" class="input-field" name="password"
+                                       placeholder="Password">
+
+
+                                <input type="submit" class="submit-btn" value="Log in">
+                            </form>
+                            <p><a class="text-blue" href="{{ route('password.request') }}">Forgot Your
+                                Password?</a></p>
+                            <form id="register" class="input-group">
+                                <input type="text" class="input-field" placeholder="User" required>
+                                <input type="email" class="input-field" placeholder="Email" required>
+                                <input id="pwd" type="Password" class="input-field" placeholder="Password" required>
+
+                                <input type="submit" class="submit-btn" value="Register">
+                            </form>
+                        </div>
+                        <span class="sp sp-t"></span>
+                        <span class="sp sp-r"></span>
+                        <span class="sp sp-b"></span>
+                        <span class="sp sp-l"></span>
+                    </div>
                 </div>
-              </form>
+
             </div>
-          </div>
         </div>
-      </div>
-      <div class="copyright pl-0">
-        <p class="text-center">&copy; 2025 Copyright Reeji
-          
-        </p>
-      </div>
-    </div>
+
+</div>
+
+<script>
+    let x = document.getElementById("login");
+    let y = document.getElementById("register");
+    let z = document.getElementById("btn");
+    let log = document.getElementById("log");
+    let reg = document.getElementById("reg");
+    let after = document.getElementById("after");
+
+    function register() {
+        x.style.left = "-500px";
+        y.style.left = "0px";
+        z.style.left = "110px";
+        log.style.color = "rgb(234, 234, 235)";
+        reg.style.color = "#252525";
+        after.style.left = "0";
+        after.style.top = "0";
+    }
+    function login() {
+        x.style.left = "0px";
+        y.style.left = "500px";
+        z.style.left = "0px";
+        reg.style.color = "rgb(234, 234, 235)";
+        log.style.color = "#252525";
+        after.style.left = "50%";
+        after.style.top = "0";
+
+    }
+</script>
 </body>
 </html>
